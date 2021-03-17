c5caa92ffaa2fd11c929b58d2997d229ccc38b9c
liangdong.xu <liangdong.xu@leyantech.com>
2021-03-17 15:18:46 +0800

    修改：add user server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './server/user/user.module';
+
+const DbModule = MongooseModule.forRoot('mongodb://localhost/test')
 
 @Module({
-  imports: [],
+  imports: [DbModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
